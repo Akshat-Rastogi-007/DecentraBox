4d86a890e48ec75094d48fe1692a0e342dfa86b9
rastogi <akshatrastogi.jaipuriahardoi@gmail.com>
2026-04-06 21:13:00 +0530

    Added getAll Api

--- src/main/java/com/codesmashers/decentrabox/controller/FileController.java
+++ src/main/java/com/codesmashers/decentrabox/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.codesmashers.decentrabox.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,12 @@ public class FileController {
     public ResponseEntity<ApiResponseDto<?>> uploadFile(@RequestParam("file") MultipartFile file) {
         return fileService.uploadFile(file);
 
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<ApiResponseDto<?>> getAllFiles() {
+
+        return fileService.getAllUserFiles();
     }
 
 }
